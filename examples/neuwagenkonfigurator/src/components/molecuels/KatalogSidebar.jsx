f2dd0179d1e6f30745bccbc4f142b8aecfb09588
Mike Bild <mike.bild@googlemail.com>
2019-09-19 13:43:51 +0200

    styles

--- examples/neuwagenkonfigurator/src/components/molecuels/KatalogSidebar.jsx
+++ examples/neuwagenkonfigurator/src/components/molecuels/KatalogSidebar.jsx
@@ -1,20 +1,44 @@
 import React from 'react';
+import './KatalogSidebar.css';
 
 export const katalogEnum = {
   FAHRZEUGE: 'fahrzeuge',
   BAUTEILE: 'bauteile',
-}
+};
 
-export function KatalogSidebar({activeKatalog = null, onFahrzeugeSelected = () => null, onBauteileSelected = () => null}) {
-  
+export function KatalogSidebar({
+  activeKatalog = null,
+  onFahrzeugeSelected = () => null,
+  onBauteileSelected = () => null,
+}) {
   const styleSelected = { backgroundColor: 'peachpuff' };
   const styleDeselected = { backgroundColor: 'inherit' };
 
   return (
     <nav style={{ border: '1px solid black' }}>
       <ul>
-        <li><button style={(activeKatalog && activeKatalog === katalogEnum.FAHRZEUGE) ? styleSelected : styleDeselected} onClick={() => onFahrzeugeSelected()}>Fahrzeuge</button></li>
-        <li><button style={(activeKatalog && activeKatalog === katalogEnum.BAUTEILE) ? styleSelected : styleDeselected} onClick={() => onBauteileSelected()}>Bauteile</button></li>
+        <li>
+          <button
+            className={
+              activeKatalog && activeKatalog === katalogEnum.FAHRZEUGE
+                ? 'catalog-sidebar-button-selected'
+                : 'catalog-sidebar-button'
+            }
+            onClick={() => onFahrzeugeSelected()}>
+            Fahrzeuge
+          </button>
+        </li>
+        <li>
+          <button
+            className={
+              activeKatalog && activeKatalog === katalogEnum.BAUTEILE
+                ? 'catalog-sidebar-button-selected'
+                : 'catalog-sidebar-button'
+            }
+            onClick={() => onBauteileSelected()}>
+            Bauteile
+          </button>
+        </li>
       </ul>
     </nav>
   );
